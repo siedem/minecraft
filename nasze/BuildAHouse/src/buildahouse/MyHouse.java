fab0620cdc66b1198b4725508e9930dd1a99e0d5
siedem <siedem@users.noreply.github.com>
2014-12-11 16:31:13 +0100

    Deklaracja width i height.

--- nasze/BuildAHouse/src/buildahouse/MyHouse.java
+++ nasze/BuildAHouse/src/buildahouse/MyHouse.java
@@ -9,10 +9,8 @@
 package buildahouse;
 public class MyHouse {
   public static void build_me() {
-    // Declare width
-    // Set width to the number of blocks
-    // Declare height
-    // Set height to the number of blocks
+    int width = 20;
+    int height = 20;
 
     BuildAHouse.buildMyHouse(width, height);
   }
